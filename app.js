1a32125faa2f920db13279de5bdbd0b0e524ed48
Dawood <dawood.boland.kw@gmail.com>
2024-06-19 08:31:50 +0300

    Task Done

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 const app = express();
-const postsRoutes = require('./api/posts/posts.routes');
-const connectDb = require('./database');
+const postsRoutes = require("./api/posts/posts.routes");
+const connectDb = require("./database");
+const errorHandler = require("./Middlewares/errorHandler");
+const notFoundHandler = require("./Middlewares/notFoundHandler");
+const cors = require("cors");
 
 connectDb();
 app.use(express.json());
-app.use('/posts', postsRoutes);
+app.use(morgan("dev"));
+app.use(cors());
+app.use("/media", express.static(path.join(__dirname, "media")));
+app.use("/posts", postsRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(8000, () => {
-  console.log('The application is running on localhost:8000');
+  console.log("The application is running on localhost:8000");
 });
